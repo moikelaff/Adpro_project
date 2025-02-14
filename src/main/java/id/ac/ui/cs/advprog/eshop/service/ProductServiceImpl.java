424f8c6b52580a885be3b536380c86e7cb301ceb
Moikelaff <dylan.adiprawira@gmail.com>
2025-02-14 18:04:55 +0700

    feat: added product service implementer

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -1,5 +1,25 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
-public class ProductServiceImpl {
-    
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.repository.ProductRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    private final ProductRepository repository;
+
+    public ProductServiceImpl(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Product addProduct(Product product) {
+        return repository.save(product);
+    }
 }
