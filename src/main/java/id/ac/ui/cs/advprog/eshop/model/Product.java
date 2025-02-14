c900ba2b717604a5bd1edfa40f69df2d90498dd6
Moikelaff <dylan.adiprawira@gmail.com>
2025-02-14 18:00:36 +0700

    feat: add product model

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -0,0 +1,14 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private Long id;
+    private String name;
+    private int quantity;
+}
